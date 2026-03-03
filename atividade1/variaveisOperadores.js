32a709620d1b3cb90581060815f2676f3640158f
llype-ENG <fellypemouranasci@gmail.com>
2026-03-02 21:02:08 -0300

    Adiciona atividade1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,16 @@
+let a = 2;
+let b = 3;
+let c = a + b;
+console.log(`A soma entre ${a} e ${b} é ${c}`)
+
+c = a - b;
+console.log(`A subtração entre ${a} e ${b} é ${c}`)
+
+c = a * b;
+console.log(`A multiplicação entre ${a} e ${b} é ${c}`)
+
+c = a / b;
+console.log(`A divisão entre ${a} e ${b} é ${c}`)
+
+c = a % b;
+console.log(`O resto entre ${a} e ${b} é ${c}`)
---
+++ ola_mundo.js
@@ -0,0 +1,8 @@
+let msg ="Hello World!";
+console.log(msg);
+
+let a = 2;
+let b = 3;
+let c = a + b;
+
+console.log(`A soma emtre ${a} e ${b} é ${c}`)
