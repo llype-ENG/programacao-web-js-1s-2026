4deb8380d10708582bfc958744b6dc71ca96fbf1
llype-ENG <fellypemouranasci@gmail.com>
2026-03-09 21:29:49 -0300

    Adiciona atividade 2 do dia 2

--- atividades-dia2/atividadeEmSala.js
+++ atividades-dia2/atividadeEmSala.js
@@ -21,3 +21,29 @@ apresentarMensagemParidade(12);
 apresentarMensagemParidade(2);
 apresentarMensagemParidade(355489);
 apresentarMensagemParidade(111111110);
+
+// Atividade 2 em sala
+
+function calcularIMC(peso ,altura){
+    let imc = { 
+        valor: peso / (altura * altura),
+        categoria: ""
+    }
+
+    if(imc < 18.5){
+        imc.categoria = "baixo peso"
+    }else if(imc.valor < 24.9){
+        imc.categoria = "peso normal"
+    }else if(imc.valor < 29.9){
+        imc.categoria = "excesso de peso"
+    }else if(imc.valor < 35){
+        imc.categoria = "obesidade"
+    }else{
+        imc.categoria = "obesidade extrema"
+    }
+    
+    console.log(`Peso = ${peso}, Altura = ${altura}, IMC.valor = ${imc.valor}, IMC.categoria = ${imc.categoria}`)
+
+}
+
+calcularIMC(75, 1.80);
